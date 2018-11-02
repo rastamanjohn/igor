d4d0ff3db2881dc988220a81e653e3d944a2af27
bronderb <bronderb@gmail.com>
2018-11-02 09:09:54 -0400

    __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @Androidbotsupport.
 
 I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
 python-telegram-bot library, and am fully opensource - you can find what makes me tick \
