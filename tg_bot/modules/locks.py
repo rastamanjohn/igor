d9e81cd3f04a8e2ed62159885eedb18633abd383
bronderb <bronderb@gmail.com>
2019-01-01 16:01:59 -0500

    Revert "add videonote lock (#50)"
    This reverts commit f6cd33b65e6988216982f414bd05fb301e219afe.

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -22,7 +22,6 @@ LOCK_TYPES = {'sticker': Filters.sticker,
               'voice': Filters.voice,
               'document': Filters.document & ~Filters.animation,
               'video': Filters.video,
-              'videonote': Filters.video_note,
               'contact': Filters.contact,
               'photo': Filters.photo,
               'gif': Filters.animation,
@@ -35,7 +34,7 @@ LOCK_TYPES = {'sticker': Filters.sticker,
 
 GIF = Filters.animation
 OTHER = Filters.game | Filters.sticker | GIF
-MEDIA = Filters.audio | Filters.document | Filters.video | Filters.video_note | Filters.voice | Filters.photo
+MEDIA = Filters.audio | Filters.document | Filters.video | Filters.voice | Filters.photo
 MESSAGES = Filters.text | Filters.contact | Filters.location | Filters.venue | Filters.command | MEDIA | OTHER
 PREVIEWS = Filters.entity("url")
 
@@ -250,7 +249,6 @@ def build_lock_message(chat_id):
                    "\n - voice = `{}`" \
                    "\n - document = `{}`" \
                    "\n - video = `{}`" \
-                   "\n - videonote = `{}`" \
                    "\n - contact = `{}`" \
                    "\n - photo = `{}`" \
                    "\n - gif = `{}`" \
@@ -259,7 +257,7 @@ def build_lock_message(chat_id):
                    "\n - forward = `{}`" \
                    "\n - game = `{}`" \
                    "\n - location = `{}`".format(locks.sticker, locks.audio, locks.voice, locks.document,
-                                                 locks.video, locks.videonote, locks.contact, locks.photo, locks.gif, locks.url,
+                                                 locks.video, locks.contact, locks.photo, locks.gif, locks.url,
                                                  locks.bots, locks.forward, locks.game, locks.location)
         if restr:
             res += "\n - messages = `{}`" \
