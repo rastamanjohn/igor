03520187321a3d6c45d0b142e574ee132edf7476
bronderb <30916819+bronderb@users.noreply.github.com>
2019-02-07 08:04:33 -0500

    Change goodbye

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -7,7 +7,7 @@ from tg_bot.modules.helper_funcs.msg_types import Types
 from tg_bot.modules.sql import SESSION, BASE
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
-DEFAULT_GOODBYE = "Goodbye's Are not Forever!"
+DEFAULT_GOODBYE = "Nice knowing ya!"
 
 
 class Welcome(BASE):
